77aea09e875771df2f76d07f6d00c57a5bc261d3
Tuan Nguyen <tuanndd@gmail.com>
2022-01-09 19:46:35 +0700

    update lab

--- module-1/vagrant-quickstart/bootstrap.sh
+++ module-1/vagrant-quickstart/bootstrap.sh
@@ -11,6 +11,7 @@ sudo yum-config-manager \
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 sudo systemctl start docker
+sudo systemctl enable docker
 
 sudo usermod -aG docker vagrant
 	
